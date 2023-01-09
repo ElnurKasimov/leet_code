acb78f373020367d056c88949e137d0ee187e205
Elnur <elnur.kasimov@grand-logistic.com>
2023-01-09 17:32:56 +0200

    changed  test to parametrized

--- src/test/java/RunningSum/SolutionTest.java
+++ src/test/java/RunningSum/SolutionTest.java
@@ -1,21 +1,32 @@
 package RunningSum;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
     private final Solution solution = new Solution();
 
-    @Test
-    void testThatRunningSumZeroWorksCorrectly (){
-        //given
-        int [] currentArray = {0,0,0,0,0};
-        //when
+    @ParameterizedTest(name = "#{index} - Run test with arguments currentArray = {0} and expectedArray = {1}")
+    @MethodSource ("predefinedData")
+    void testRunningSumParameterizidWorksCorrectly (int[] currentArray, int[] expectedArray){
+        //given when
         int[] actualArray = solution.runningSum(currentArray);
         //then
-        int[] expectedArray = {0,0,0,0,0};
         assertArrayEquals(expectedArray, actualArray);
     }
 
+    static Stream<Arguments> predefinedData () {
+        return
+                Stream.of(
+                        Arguments.arguments(new int[]{0,0,0,0,0},new int[]{0,0,0,0,0}),
+                        Arguments.arguments(new int[]{1,2,3,4},new int[]{1,3,6,10}),
+                        Arguments.arguments(new int[]{-1,0,1,-1,1},new int[]{-1,-1,0,-1,0})
+                );
+    }
 }
