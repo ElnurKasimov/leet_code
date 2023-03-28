1b03b28a95124e2fed95c7d11e7d062f714d6952
Elnur <elnur.kasimov@grand-logistic.com>
2023-03-28 14:57:04 +0300

    solved HighestDepartmentSalary, solution of LeetCode was added in comments

--- src/main/java/medium/HighestDepartmentSalary.java
+++ src/main/java/medium/HighestDepartmentSalary.java
@@ -4,15 +4,15 @@ public class HighestDepartmentSalary {
 
 //    TASK - 184. Department Highest Salary
 //
-//    TEMPORARY SOLUTION
-//    SELECT d.name Department, max(e.salary) Salary
-//    FROM Employee e LEFT JOIN Department d ON d.id = e.departmentid
-//    GROUP BY d.id;
-//
-//    WRONG SOLUTION
-//    SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+//    MY SOLUTION
+//    SELECT d.name AS Department, e.name AS Employee, salary AS Salary
+//    FROM Employee AS e LEFT JOIN Department AS d ON d.id = e.departmentId
+//    WHERE e.salary = (select max(salary) from employee WHERE departmentId = d.id group BY departmentId);
+
+//    LEET CODE SOLUTION
+//    SELECT d.name AS Department, e.name AS Employee, salary AS Salary
 //    FROM Employee AS e LEFT JOIN Department AS d ON d.id = e.departmentid
-//    WHERE e.salary IN (select max(salary) from employee group BY departmentId);
+//    WHERE (e.departmentId, salary) IN (select departmentId,max(salary) from employee group BY departmentId);
 
 
 //    POPULATION
@@ -41,6 +41,5 @@ public class HighestDepartmentSalary {
 //            (3,'Henry',80000,2),
 //            (4,'Sam',60000,2),
 //            (5,'Max',90000,1);
-
 
 }
