55a07a4f1b689fa77a7be258f591234c73eedd3f
Elnur <elnur.kasimov@grand-logistic.com>
2023-04-04 17:12:17 +0300

    created SecondHighestSalary

--- src/main/java/medium/SecondHighestSalary.java
+++ src/main/java/medium/SecondHighestSalary.java
@@ -1,25 +1,12 @@
 package medium;
 
-public class HighestDepartmentSalary {
-//    TASK - 184. Department Highest Salary
-
+public class SecondHighestSalary {
 //    MY SOLUTION
 //    SELECT d.name AS Department, e.name AS Employee, salary AS Salary
 //    FROM Employee AS e LEFT JOIN Department AS d ON d.id = e.departmentId
 //    WHERE e.salary = (select max(salary) from employee WHERE departmentId = d.id group BY departmentId);
 
 //    LEET CODE SOLUTION
-//    SELECT d.name AS Department, e.name AS Employee, salary AS Salary
-//    FROM Employee AS e LEFT JOIN Department AS d ON d.id = e.departmentI
-//    WHERE (Employee.DepartmentId , Salary)
-//    IN
-//    (   SELECT
-//            DepartmentId, MAX(Salary)
-//        FROM
-//            Employee
-//        GROUP BY DepartmentId
-//    )
-//);
 
 //    POPULATION
 //    CREATE TABLE Employee (
@@ -42,5 +29,6 @@ public class HighestDepartmentSalary {
 //            (3,'Henry',80000,2),
 //            (4,'Sam',60000,2),
 //            (5,'Max',90000,1);
+
 
 }
