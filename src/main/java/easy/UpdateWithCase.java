ba0a9c072d5f572d090c4bad042cf3e34ab9f74d
Elnur <elnur.kasimov@grand-logistic.com>
2023-04-06 17:26:42 +0300

    created UpdateWithCase

---
+++ src/main/java/easy/UpdateWithCase.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class UpdateWithCase {
+
+//    TASK - 627. Swap Salary
+
+//    SOLUTION
+
+
+//    LEET CODE SOLUTION - doesn't work in POstgreSQL
+//    UPDATE salary SET sex = CASE sex
+//                              WHEN 'm' THEN 'f'
+//                              ELSE 'm'
+//                            END;
+
+}
