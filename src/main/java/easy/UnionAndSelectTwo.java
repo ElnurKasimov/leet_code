ae2b940c7b5edd85ac99a07b2cba9c241d5e85b2
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-10 23:31:32 +0300

    created UnionAndSelectTwo

---
+++ src/main/java/easy/UnionAndSelectTwo.java
@@ -0,0 +1,17 @@
+package easy;
+public class UnionAndSelectTwo {
+
+//    TASK - 1795. Rearrange Products Table
+
+//    SOLUTION
+//
+
+//    LEET CODE SOLUTION
+//
+
+
+//       POPULATION
+//
+
+
+}
