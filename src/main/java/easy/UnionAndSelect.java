8c129619e288b942a089281040e99c2fd1aaef9d
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-10 23:28:48 +0300

    created UnionAndSelect

---
+++ src/main/java/easy/UnionAndSelect.java
@@ -0,0 +1,12 @@
+package easy;
+public class UnionAndSelect {
+
+//    TASK - 1965. Employees With Missing Information1667. Fix Names in a Table
+
+//    SOLUTION
+//
+
+//    LEET CODE SOLUTION
+//
+
+}
