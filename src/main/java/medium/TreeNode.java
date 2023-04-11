61e51baa9699ed0b53972c2af42b2b21ba434497
Elnur <elnur.kasimov@grand-logistic.com>
2023-04-11 17:59:08 +0300

    created TreeNode

---
+++ src/main/java/medium/TreeNode.java
@@ -0,0 +1,31 @@
+package medium;
+public class TreeNode {
+
+//    TASK - 1795. Rearrange Products Table
+
+//    SOLUTION
+//  SELECT product_id,'store1' as store, store1 as price FROM Products WHERE store1 IS NOT NULL
+//  UNION
+//  SELECT product_id,'store2' as store, store2 as price FROM Products WHERE store2 IS NOT NULL
+//  UNION
+//  SELECT product_id,'store3' as store, store3 as price FROM Products WHERE store3 IS NOT NULL
+//  ORDER BY product_id;
+
+//    LEET CODE SOLUTION
+//
+
+
+//       POPULATION
+//  CREATE TABLE Products (
+//	    product_id INTEGER PRIMARY KEY,
+//	    store1 INTEGER,
+//	    store2 INTEGER,
+//	    store3 INTEGER
+//  );
+//
+//  INSERT INTO Products(product_id, store1, store2, store3) VALUES
+//	    (0, 95, 100, 105),
+//	    (1, 70, null, 80);
+
+
+}
