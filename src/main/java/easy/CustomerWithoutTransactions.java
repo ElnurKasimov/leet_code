a4afa4c7f447ef51550f0d3395d4f06bded4502a
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-12 00:15:07 +0300

    renamed CustomerWithoutTransactions

---
+++ src/main/java/easy/CustomerWithoutTransactions.java
@@ -0,0 +1,16 @@
+package easy;
+public class CustomerWithoutTransactions {
+
+//    TASK - 1581. Customer Who Visited but Did Not Make Any Transactions
+
+//    SOLUTION
+
+//    LEET CODE SOLUTION
+//
+
+
+//       POPULATION
+
+
+
+}
