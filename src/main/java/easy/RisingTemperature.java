9bb91e2506ceb101b9e03da49d706f98cc88f146
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-16 14:56:22 +0300

    created RisingTemperature

---
+++ src/main/java/easy/RisingTemperature.java
@@ -0,0 +1,25 @@
+package easy;
+
+public class RisingTemperature {
+
+ //    TASK  - 197. Rising Temperature
+
+//    MY SOLUTION
+//
+
+//    LEET-CODE SOLUTION
+
+
+//    POPULATION
+
+//    CREATE TABLE Weather (
+//            id INTEGER PRIMARY KEY,
+//            recordDate DATE,
+//            temperature INTEGER);
+//    INSERT INTO Weather (id,recordDate,temperature) VALUES
+//	           (1, '2015-01-01', 10),
+//            (2,'2015-01-02',25),
+//            (3,'2015-01-03', 20),
+//            (4,'2015-01-04',30);
+
+}
