198ab99295fe059962350c12774d6d81af49af77
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-16 15:53:10 +0300

    solved  RisingTemperature but solution doesn't work in MySQL

--- src/main/java/easy/RisingTemperature.java
+++ src/main/java/easy/RisingTemperature.java
@@ -4,10 +4,19 @@ public class RisingTemperature {
 
  //    TASK  - 197. Rising Temperature
 
-//    MY SOLUTION
-//
+//    MY SOLUTION - does't work in MySQL
+//  SELECT w.id FROM Weather JOIN Weather AS w ON Weather.recordDate = w.recordDate - INTEGER '1'
+//   WHERE Weather.temperature < w.temperature;
 
 //    LEET-CODE SOLUTION
+//    SELECT
+//    weather.id AS 'Id'
+//    FROM
+//            weather
+//    JOIN
+//    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+//    AND weather.Temperature > w.Temperature
+//    ;
 
 
 //    POPULATION
