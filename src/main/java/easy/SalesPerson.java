a4f5d86aea3e4108647ab1fb4f1e3a5981fc1bfe
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-18 00:00:16 +0300

    trying to solve SalesPerson - doesn't work

--- src/main/java/easy/SalesPerson.java
+++ src/main/java/easy/SalesPerson.java
@@ -6,6 +6,9 @@ public class SalesPerson {
 
 
 //    MY SOLUTION
+//SELECT s.name FROM Orders AS o RIGHT JOIN SalesPerson AS s ON o.sales_id = s.sales_id
+//    RIGHT JOIN Company c ON o.com_id = c.com_id WHERE
+//    c.name LIKE 'RED' AND order_id IS NULL;
 
 //    LEET-CODE SOLUTION
 
