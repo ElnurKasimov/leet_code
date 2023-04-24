bbc57aa278f8b2e5d4d6db097fed1d04f57517cc
ElnurKasimov <ekasimov2012@gmail.com>
2023-04-24 22:39:40 +0300

    created DailyLeadsAndPartners

---
+++ src/main/java/easy/SQL/DailyLeadsAndPartners.java
@@ -0,0 +1,18 @@
+package easy.SQL;
+
+public class DailyLeadsAndPartners {
+
+ //    TASK  - 1693. Daily Leads and Partners
+
+
+//    MY SOLUTION
+
+
+//    LEET-CODE SOLUTION
+
+
+
+//    POPULATION
+
+
+}
